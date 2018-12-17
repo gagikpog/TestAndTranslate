9ba4b83514003d84450250d4bcbc7af307259eea
Gagik <gagikpog@gmail.com>
2018-12-17 13:32:37 +0500

    create main form

---
+++ Debug/ui_mainwindow.h
@@ -0,0 +1,293 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.9.5
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QLocale>
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout_2;
+    QSpacerItem *horizontalSpacer;
+    QSpacerItem *verticalSpacer;
+    QHBoxLayout *horizontalLayout_2;
+    QPushButton *pushButton_6;
+    QSpacerItem *horizontalSpacer_3;
+    QVBoxLayout *verticalLayout;
+    QSpacerItem *verticalSpacer_2;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_5;
+    QSpacerItem *verticalSpacer_3;
+    QSpacerItem *horizontalSpacer_4;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->setWindowModality(Qt::NonModal);
+        MainWindow->setEnabled(true);
+        MainWindow->resize(485, 649);
+        MainWindow->setMinimumSize(QSize(400, 0));
+        MainWindow->setMaximumSize(QSize(600, 800));
+        QFont font;
+        font.setFamily(QStringLiteral("Carlito"));
+        font.setPointSize(12);
+        font.setBold(false);
+        font.setWeight(50);
+        MainWindow->setFont(font);
+        MainWindow->setStyleSheet(QStringLiteral(""));
+        MainWindow->setLocale(QLocale(QLocale::Russian, QLocale::Russia));
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        centralWidget->setEnabled(true);
+        QPalette palette;
+        QRadialGradient gradient(0.3, -0.4, 1.3, 0.3, -0.4);
+        gradient.setSpread(QGradient::PadSpread);
+        gradient.setCoordinateMode(QGradient::ObjectBoundingMode);
+        gradient.setColorAt(0, QColor(255, 255, 255, 255));
+        gradient.setColorAt(1, QColor(170, 170, 170, 255));
+        QBrush brush(gradient);
+        palette.setBrush(QPalette::Active, QPalette::Button, brush);
+        QRadialGradient gradient1(0.3, -0.4, 1.3, 0.3, -0.4);
+        gradient1.setSpread(QGradient::PadSpread);
+        gradient1.setCoordinateMode(QGradient::ObjectBoundingMode);
+        gradient1.setColorAt(0, QColor(255, 255, 255, 255));
+        gradient1.setColorAt(1, QColor(170, 170, 170, 255));
+        QBrush brush1(gradient1);
+        palette.setBrush(QPalette::Active, QPalette::Base, brush1);
+        QRadialGradient gradient2(0.3, -0.4, 1.3, 0.3, -0.4);
+        gradient2.setSpread(QGradient::PadSpread);
+        gradient2.setCoordinateMode(QGradient::ObjectBoundingMode);
+        gradient2.setColorAt(0, QColor(255, 255, 255, 255));
+        gradient2.setColorAt(1, QColor(170, 170, 170, 255));
+        QBrush brush2(gradient2);
+        palette.setBrush(QPalette::Active, QPalette::Window, brush2);
+        QRadialGradient gradient3(0.3, -0.4, 1.3, 0.3, -0.4);
+        gradient3.setSpread(QGradient::PadSpread);
+        gradient3.setCoordinateMode(QGradient::ObjectBoundingMode);
+        gradient3.setColorAt(0, QColor(255, 255, 255, 255));
+        gradient3.setColorAt(1, QColor(170, 170, 170, 255));
+        QBrush brush3(gradient3);
+        palette.setBrush(QPalette::Inactive, QPalette::Button, brush3);
+        QRadialGradient gradient4(0.3, -0.4, 1.3, 0.3, -0.4);
+        gradient4.setSpread(QGradient::PadSpread);
+        gradient4.setCoordinateMode(QGradient::ObjectBoundingMode);
+        gradient4.setColorAt(0, QColor(255, 255, 255, 255));
+        gradient4.setColorAt(1, QColor(170, 170, 170, 255));
+        QBrush brush4(gradient4);
+        palette.setBrush(QPalette::Inactive, QPalette::Base, brush4);
+        QRadialGradient gradient5(0.3, -0.4, 1.3, 0.3, -0.4);
+        gradient5.setSpread(QGradient::PadSpread);
+        gradient5.setCoordinateMode(QGradient::ObjectBoundingMode);
+        gradient5.setColorAt(0, QColor(255, 255, 255, 255));
+        gradient5.setColorAt(1, QColor(170, 170, 170, 255));
+        QBrush brush5(gradient5);
+        palette.setBrush(QPalette::Inactive, QPalette::Window, brush5);
+        QRadialGradient gradient6(0.3, -0.4, 1.3, 0.3, -0.4);
+        gradient6.setSpread(QGradient::PadSpread);
+        gradient6.setCoordinateMode(QGradient::ObjectBoundingMode);
+        gradient6.setColorAt(0, QColor(255, 255, 255, 255));
+        gradient6.setColorAt(1, QColor(170, 170, 170, 255));
+        QBrush brush6(gradient6);
+        palette.setBrush(QPalette::Disabled, QPalette::Button, brush6);
+        QRadialGradient gradient7(0.3, -0.4, 1.3, 0.3, -0.4);
+        gradient7.setSpread(QGradient::PadSpread);
+        gradient7.setCoordinateMode(QGradient::ObjectBoundingMode);
+        gradient7.setColorAt(0, QColor(255, 255, 255, 255));
+        gradient7.setColorAt(1, QColor(170, 170, 170, 255));
+        QBrush brush7(gradient7);
+        palette.setBrush(QPalette::Disabled, QPalette::Base, brush7);
+        QRadialGradient gradient8(0.3, -0.4, 1.3, 0.3, -0.4);
+        gradient8.setSpread(QGradient::PadSpread);
+        gradient8.setCoordinateMode(QGradient::ObjectBoundingMode);
+        gradient8.setColorAt(0, QColor(255, 255, 255, 255));
+        gradient8.setColorAt(1, QColor(170, 170, 170, 255));
+        QBrush brush8(gradient8);
+        palette.setBrush(QPalette::Disabled, QPalette::Window, brush8);
+        centralWidget->setPalette(palette);
+        centralWidget->setStyleSheet(QLatin1String("QWidget {\n"
+"background: qradialgradient(cx: 0.3, cy: -0.4,\n"
+"fx: 0.3, fy: -0.4,\n"
+"radius: 1.3, stop: 0 #fff, stop: 1 #aaa);\n"
+"}\n"
+"\n"
+"QPushButton {\n"
+"color: #000;\n"
+"border: 2px solid #555;\n"
+"border-radius: 17px;\n"
+"padding: 5px;\n"
+"background: qradialgradient(cx: 0.3, cy: -0.4,\n"
+"fx: 0.3, fy: -0.4,\n"
+"radius: 1.3, stop: 0 #fff, stop: 1 #ccc);\n"
+"}\n"
+"QPushButton:hover {\n"
+"background: qradialgradient(cx: 0.3, cy: -0.4,\n"
+"fx: 0.3, fy: -0.4,\n"
+"radius: 1.3, stop: 0 #fff, stop: 1 #fff);\n"
+"}\n"
+"QPushButton:pressed {\n"
+"background: qradialgradient(cx: 0.4, cy: -0.1,\n"
+"fx: 0.4, fy: -0.1,\n"
+"radius: 1.3, stop: 0 #eee, stop: 1 #aaa);\n"
+"}"));
+        horizontalLayout = new QHBoxLayout(centralWidget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setSpacing(6);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        verticalLayout_2->addItem(horizontalSpacer);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_2->addItem(verticalSpacer);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        pushButton_6 = new QPushButton(centralWidget);
+        pushButton_6->setObjectName(QStringLiteral("pushButton_6"));
+        pushButton_6->setMinimumSize(QSize(40, 40));
+        pushButton_6->setMaximumSize(QSize(40, 40));
+
+        horizontalLayout_2->addWidget(pushButton_6);
+
+        horizontalSpacer_3 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_2->addItem(horizontalSpacer_3);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_2);
+
+
+        horizontalLayout->addLayout(verticalLayout_2);
+
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setSpacing(20);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalSpacer_2 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer_2);
+
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setMinimumSize(QSize(300, 90));
+        pushButton->setMaximumSize(QSize(400, 100));
+        QFont font1;
+        font1.setPointSize(18);
+        pushButton->setFont(font1);
+        pushButton->setCursor(QCursor(Qt::PointingHandCursor));
+        pushButton->setMouseTracking(false);
+        pushButton->setStyleSheet(QStringLiteral(""));
+
+        verticalLayout->addWidget(pushButton);
+
+        pushButton_2 = new QPushButton(centralWidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setMinimumSize(QSize(300, 90));
+        pushButton_2->setMaximumSize(QSize(400, 100));
+        pushButton_2->setFont(font1);
+        pushButton_2->setCursor(QCursor(Qt::PointingHandCursor));
+        pushButton_2->setMouseTracking(false);
+        pushButton_2->setStyleSheet(QStringLiteral(""));
+
+        verticalLayout->addWidget(pushButton_2);
+
+        pushButton_3 = new QPushButton(centralWidget);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+        pushButton_3->setMinimumSize(QSize(300, 90));
+        pushButton_3->setMaximumSize(QSize(400, 100));
+        pushButton_3->setFont(font1);
+        pushButton_3->setCursor(QCursor(Qt::PointingHandCursor));
+        pushButton_3->setMouseTracking(false);
+        pushButton_3->setStyleSheet(QStringLiteral(""));
+
+        verticalLayout->addWidget(pushButton_3);
+
+        pushButton_4 = new QPushButton(centralWidget);
+        pushButton_4->setObjectName(QStringLiteral("pushButton_4"));
+        pushButton_4->setMinimumSize(QSize(300, 90));
+        pushButton_4->setMaximumSize(QSize(400, 100));
+        pushButton_4->setFont(font1);
+        pushButton_4->setCursor(QCursor(Qt::PointingHandCursor));
+        pushButton_4->setMouseTracking(false);
+        pushButton_4->setStyleSheet(QStringLiteral(""));
+
+        verticalLayout->addWidget(pushButton_4);
+
+        pushButton_5 = new QPushButton(centralWidget);
+        pushButton_5->setObjectName(QStringLiteral("pushButton_5"));
+        pushButton_5->setMinimumSize(QSize(300, 90));
+        pushButton_5->setMaximumSize(QSize(400, 100));
+        pushButton_5->setFont(font1);
+        pushButton_5->setCursor(QCursor(Qt::PointingHandCursor));
+
+        verticalLayout->addWidget(pushButton_5);
+
+        verticalSpacer_3 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer_3);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        horizontalSpacer_4 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_4);
+
+        MainWindow->setCentralWidget(centralWidget);
+
+        retranslateUi(MainWindow);
+        QObject::connect(pushButton_5, SIGNAL(clicked()), MainWindow, SLOT(close()));
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        pushButton_6->setText(QApplication::translate("MainWindow", "RU", Q_NULLPTR));
+        pushButton->setText(QApplication::translate("MainWindow", "PushButton", Q_NULLPTR));
+        pushButton_2->setText(QApplication::translate("MainWindow", "Training", Q_NULLPTR));
+        pushButton_3->setText(QApplication::translate("MainWindow", "Translate", Q_NULLPTR));
+        pushButton_4->setText(QApplication::translate("MainWindow", "Settings", Q_NULLPTR));
+        pushButton_5->setText(QApplication::translate("MainWindow", "Exit", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
